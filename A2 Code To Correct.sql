36514f94ad1a5237818d354558abdada655253fe
Pratham Patel <prathamp0422@gmail.com>
2025-10-28 12:08:31 -0600

    added error and comment from line 40-74

--- A2 Code To Correct.sql
+++ A2 Code To Correct.sql
@@ -37,14 +37,15 @@ BEGIN
           UPDATE gggs_customer
              SET status = r_gggs.column2
            WHERE name = r_gggs.column1;
-            
+          
         ELSIF (r_gggs.process_type = k_change) THEN
           UPDATE gggs_customer
              SET province = DECODE(r_gggs.column2, k_no_change_char, province, r_gggs.column2),
                  first_name = DECODE(r_gggs.column3, k_no_change_char, first_name, r_gggs.column3),
                  last_name = DECODE(r_gggs.column4, k_no_change_char, last_name, r_gggs.column4),
                  city = DECODE(r_gggs.column5, k_no_change_char, city, r_gggs.column5),
-                 phone_number = NVL(r_gggs.column6, r_gggs.column6, phone_number)
+                --  phone_number = NVL(r_gggs.column6, r_gggs.column6, phone_number)
+                 phone_number = NVL(r_gggs.column6, phone_number) -- change NVL given only 2 expression (syntax)
            WHERE name = r_gggs.column1;  
    	    ELSE 
 	      RAISE_APPLICATION_ERROR(-20001, r_gggs.process_type || ' is not a valid process request for ' || r_gggs.data_type || ' data');
@@ -55,9 +56,9 @@ BEGIN
         IF (r_gggs.process_type = k_new) THEN
           INSERT INTO gggs_vendor
           VALUES (gggs_vendor_seq.NEXTVAL, r_gggs.column1, r_gggs.column2, r_gggs.column3,
-                  r_gggs.column4, r_gggs.column6, k_status);      
+                  r_gggs.column4, r_gggs.column6, k_active_status); -- change k_status should be k_active_status  (logic)
                 
-        ELSIF (r_gggs.process_type = k_stats) THEN
+        ELSIF (r_gggs.process_type = k_status) THEN  -- typo change k_stats to k_status (syntax)
           UPDATE gggs_vendor
              SET status = r_gggs.column2
            WHERE name = r_gggs.column1;    
@@ -68,11 +69,11 @@ BEGIN
                  contact_first_name = DECODE(r_gggs.column3, k_no_change_char, contact_first_name, r_gggs.column3),
                  contact_last_name = DECODE(r_gggs.column4, k_no_change_char, contact_last_name, r_gggs.column4),
                  contact_phone_number = NVL2(r_gggs.column6, r_gggs.column6, contact_phone_number)
-           WHERE name = r_gggs.column1  
+           WHERE name = r_gggs.column1; -- added semicolon (syntax)
         ELSE 
 	      RAISE_APPLICATION_ERROR(-20001, r_gggs.process_type || ' is not a valid process request for ' || r_gggs.data_type || ' data');
         END IF;
-
+ 
       ELSIF (r_gggs.data_type = k_category) THEN
 
         IF (r_gggs.process_type = k_new) THEN
@@ -109,7 +110,7 @@ BEGIN
              SET status = r_gggs.column2
            WHERE name = r_gggs.column1;
       
-        ELSE IF (r_gggs.process_type = k_change) THEN
+        ELSE IF (r_gggs.process_type = k_change) THEN -- fix this and srcipt should run (ELSE IF -> ELSIF)
           UPDATE gggs_stock
              SET description = DECODE(r_gggs.column4, k_no_change_char, description, r_gggs.column4),
                  price = NVL2(r_gggs.column7, r_gggs.column7, price),
@@ -118,7 +119,7 @@ BEGIN
         ELSE 
 	      RAISE_APPLICATION_ERROR(-20001, r_gggs.data_type || ' is not a valid process request for ' || r_gggs.process_type || ' data');
         END IF;
-	  ELSE 
+	    ELSE 
 	    RAISE_APPLICATION_ERROR(-20000, r_gggs.data_type || ' is not a valid type of data to process');
       END IF;
     
@@ -139,6 +140,7 @@ BEGIN
          (r_gggs.data_type, r_gggs.process_type, v_message);
 	   
 	    COMMIT; 
+      END; -- added a end statement to close off the exception section
 
 	   
   END LOOP;  
